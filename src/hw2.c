6c409b960a2d5befce01585f8d8bd0c970fd1438
sammi <158765072+samm1c@users.noreply.github.com>
2024-09-30 00:54:14 +0000

    p1 r4 (realized i didn't need to do BE)

--- src/hw2.c
+++ src/hw2.c
@@ -42,25 +42,25 @@ void print_packet(unsigned int packet[]) // print the packet's information
 
      printf("Data: ");
      if (type == 0) {
-          bool keep[4]; // list to keep track of which bytes to keep for first and last BE
-          for (int i = 0; i < 4; i++) { // initialize keep
-               keep[i] = false;
-          }
+          //bool keep[4]; // list to keep track of which bytes to keep for first and last BE
+          // for (int i = 0; i < 4; i++) { // initialize keep
+          //      keep[i] = false;
+          // }
           // first BE | (packet[3])
-          if ((first_BE & 1) == 1) { // 1 -> 0001 ; first byte
-               keep[0] = true;
-          }
-          if ((first_BE & 2) == 2)  { // 2 -> 0010 ; second byte
-               keep[1] = true;
-          }
-          if ((first_BE & 4) == 4)  { // 4 -> 0100 ; third byte
-               keep[2] = true;
-          }
-          if ((first_BE & 8) == 8)  { // 8 -> 1000 ; fourth byte
-               keep[3] = true;
-          }
+          // if ((first_BE & 1) == 1) { // 1 -> 0001 ; first byte
+          //      keep[0] = true;
+          // }
+          // if ((first_BE & 2) == 2)  { // 2 -> 0010 ; second byte
+          //      keep[1] = true;
+          // }
+          // if ((first_BE & 4) == 4)  { // 4 -> 0100 ; third byte
+          //      keep[2] = true;
+          // }
+          // if ((first_BE & 8) == 8)  { // 8 -> 1000 ; fourth byte
+          //      keep[3] = true;
+          // }
           //int first_word = packet[3]; // variable needed b/c will use bitwise operators on it
-          int j = 0;
+          //int j = 0;
           // for (int i = 0; i < 4 && j < 4; i++) {
           //      if (keep[i]) { // keep going until you hit a false and print the whole thing out
           //           j = i; // second counter
@@ -72,38 +72,38 @@ void print_packet(unsigned int packet[]) // print the packet's information
           //      } // otherwise don't do anything if there's no true
           // }
           // middle data words
-          for (int i = 3; i < length + 3 - 1; i++) {
+          for (int i = 3; i <= length + 3 - 1; i++) {
                printf("%d ", packet[i]);
           }
           //clear the "keep" list
-          for (int k = 0; k < 4; k++) {
-               keep[k] = false;
-          }
+          // for (int k = 0; k < 4; k++) {
+          //      keep[k] = false;
+          // }
           // last BE (packet[N+3])
-          if ((last_BE & 1) == 1) { // 1 -> 0001 ; first byte
-               keep[0] = true;
-          }
-          if ((last_BE & 2) == 2)  { // 2 -> 0010 ; second byte
-               keep[1] = true;
-          }
-          if ((last_BE & 4) == 4)  { // 4 -> 0100 ; third byte
-               keep[2] = true;
-          }
-          if ((last_BE & 8) == 8)  { // 8 -> 1000 ; fourth byte
-               keep[3] = true;
-          }
-          int last_word = packet[length + 3 - 1];
-          j = 0; // reset
-          for (int i = 0; i < 4 && j < 4; i++) { // same as before
-               if (keep[i]) {
-                    j = i;
-                    while (j < 4 && keep[j]) {
-                         j++;
-                    }
-                    last_word = last_word >> (8*j);
-                    printf("%d ", last_word);
-               }
-          }
+          // if ((last_BE & 1) == 1) { // 1 -> 0001 ; first byte
+          //      keep[0] = true;
+          // }
+          // if ((last_BE & 2) == 2)  { // 2 -> 0010 ; second byte
+          //      keep[1] = true;
+          // }
+          // if ((last_BE & 4) == 4)  { // 4 -> 0100 ; third byte
+          //      keep[2] = true;
+          // }
+          // if ((last_BE & 8) == 8)  { // 8 -> 1000 ; fourth byte
+          //      keep[3] = true;
+          // }
+          // int last_word = packet[length + 3 - 1];
+          // j = 0; // reset
+          // for (int i = 0; i < 4 && j < 4; i++) { // same as before
+          //      if (keep[i]) {
+          //           j = i;
+          //           while (j < 4 && keep[j]) {
+          //                j++;
+          //           }
+          //           last_word = last_word >> (8*j);
+          //           printf("%d ", last_word);
+          //      }
+          // }
      }
      printf("\n");
 }
