0ca21871b9cc674ab60bfb113ca652d5becebe4a
sammi <158765072+samm1c@users.noreply.github.com>
2024-10-01 00:32:09 +0000

    p3 r1 (testing just to see)

--- src/hw2.c
+++ src/hw2.c
@@ -103,71 +103,95 @@ void store_values(unsigned int packets[], char *memory)
                     i++; // next row in packet
                }
           }
-          // finish checking the data
+          // finished checking the data
      }
 }
 
 unsigned int* create_completion(unsigned int packets[], const char *memory)
 {
-    (void)packets;
-    (void)memory;
-	return NULL;
+     int i = 0; // index of packets
+     int m = 0; // index of *memory
+     int c = 0; // index of completion packets array
+     // int p = 0; // number of packets
+     // int num_data = 0; // number of total data
+
+     int length, id, tag, last_BE, first_BE, address;
+     int total_length = 0; // must calculate the total number of bytes needed for completion packets first before doing anything else
+     bool valid_packet = true;
+
+     // calculate total_length and check valid packet
+     int j = 0; // special index for this loop (calculating length)
+     while (valid_packet) {
+          if (packets[j] >> 10 != 0) { // int[0] must all be 0 after the length; check for valid read type packet
+               return NULL; // error
+          }
+          total_length += packets[j] & 1023; // 2^10 - 1 b/c you only want last 10 bits for length
+          j += 3; // skip rest of the header into the next packet
+     }
+
+     // create and initialize completion packets array (size does not need to be fixed b/c we are malloc'ing it)
+     unsigned int *completion = (unsigned int *)malloc(sizeof(int) * total_length); 
+
+     while (valid_packet) {
+          
+          length = packets[i] & 1023; // 2^10 - 1 = 1023 b/c you only want last 10 bits for length
+          i++; // done with int[0]
+          id = packets[i] >> 16;
+          tag = (packets[i] >> 8) & 255; // get rid of last 8 bits; 2^8 - 1 = 255 for last 8 bits
+          last_BE = (packets[i] >> 4) & 15; // get rid of last 4; 2^4 - 1 = 15 for last 4 bits
+          first_BE = packets[i] & 15; // 2^4 - 1 = 15 for last 4 bits
+          i++; // done with int[1]
+          if (packets[i] > 1000000) { // check address does not exceed 1MB memory
+               return NULL;
+          }
+          address = packets[i];
+          i++; // done with int[2]
+          // finished reading the read request
+
+          completion[c] = ((37) << 25) | length; // hopefully this means that im shifting 37 25 bits to the left and add the length to the last 10
+          c++; // done with completion int[0]
+          completion[c] = (220 << 16) | (length * 4); // shift 220 to the left 16 bits and add the byte count to the last 12
+          c++; // done with completion int[1]
+          completion[c] = (id << 16) | (tag << 8) | (length * 4);
+          c++; // done with completion int[2]
+
+          m = address;
+
+          // start reading memory array into completion packet
+          for (int a = 0; a < length; a++) { // length times; a has no bearing
+               
+               if (a == 0 || a == length - 1) {
+                    int byte_enable;
+                    if (a == 0) {
+                         byte_enable = first_BE;
+                    } else if (a == length - 1) {
+                         byte_enable = last_BE;
+                    }
+
+                    for (int k = 0; k < 4; k++) { // 4 times; k has no bearing
+                         if ((byte_enable & 1) == 1) {
+                              completion[c] |= (memory[m] << (8 * k));
+                         }
+                         byte_enable = byte_enable >> 1;
+                         m++;
+                    }
+                    c++;
+               } else {
+                    for (int k = 0; k < 4; k++) { // 4 times; k has no bearing
+                         completion[c] |= (memory[m] << (8 * k));
+                         m++;
+                    }
+                    c++;
+               }
+
+          }
+
+          //p++; // this is a valid packet
+     }
+     //create completion packets for this packet you're reading and return them by using malloc() so that they can persist
+     return completion;
+
+    //(void)packets;
+    //(void)memory;
+	//return NULL;
 }
-
-
-// bool keep[4]; // tracks which bits to keep
-                    // for (int k = 0; k < 4; k++) { //
-                    //      keep[k] = false;
-                    // }
-                    // if (a == address) { // first BE
-                    //      if ((first_BE & 1) == 1) { // 1 -> 0001 ; first byte
-                    //           keep[0] = true;
-                    //      }
-                    //      if ((first_BE & 2) == 2)  { // 2 -> 0010 ; second byte
-                    //           keep[1] = true;
-                    //      }
-                    //      if ((first_BE & 4) == 4)  { // 4 -> 0100 ; third byte
-                    //           keep[2] = true;
-                    //      }
-                    //      if ((first_BE & 8) == 8)  { // 8 -> 1000 ; fourth byte
-                    //           keep[3] = true;
-                    //      }
-                    //      int first_word = packets[i]; // variable needed b/c will use bitwise operators on it
-                    //      int b = 0;
-                    //      for (int a = 0; a < 4 && b < 4; a++) {
-                    //           if (keep[a]) { // keep going until you hit a false and store whole thing
-                    //                b = a; // second counter
-                    //                while (b < 4 && keep[b]) {
-                    //                     b++;
-                    //                }
-                    //                first_word = first_word >> (8*b); // skip b bytes
-                    //                ((unsigned int *)memory)[j] = first_word;
-                    //           } // otherwise don't do anything if there's no true
-                    //      }
-                    // } else if (a == length - 1) {
-                    //      if ((last_BE & 1) == 1) { // 1 -> 0001 ; first byte
-                    //           keep[0] = true;
-                    //      }
-                    //      if ((last_BE & 2) == 2)  { // 2 -> 0010 ; second byte
-                    //           keep[1] = true;
-                    //      }
-                    //      if ((last_BE & 4) == 4)  { // 4 -> 0100 ; third byte
-                    //           keep[2] = true;
-                    //      }
-                    //      if ((last_BE & 8) == 8)  { // 8 -> 1000 ; fourth byte
-                    //           keep[3] = true;
-                    //      }
-                    //      int last_word = packets[i];
-                    //      int b = 0;
-                    //      for (int a = 0; a < 4 && b < 4; a++) { // same as before
-                    //           if (keep[a]) {
-                    //                b = a;
-                    //                while (j < 4 && keep[b]) {
-                    //                     b++;
-                    //                }
-                    //                last_word = last_word >> (8*b);
-                    //                ((unsigned int *)memory)[j] = last_word;
-                    //           }
-                    //      }
-                    // }
-                    // i++; // move to next row in packet
